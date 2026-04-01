1f14a1df778b745c0a5ca1ed5d140fc5528419cd
Leon <leon.fernandez@internetstiftelsen.se>
2026-04-01 13:18:01 +0200

    Use shared nats client library
    Use new, singleton libtapir

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/dnstapir/tapir-analyse-lib/common"
+	"github.com/dnstapir/tapir-analyse-lib/libtapir"
 )
 
 const c_N_HANDLERS = 3
@@ -18,9 +19,9 @@ type Conf struct {
 	Debug               bool   `toml:"debug"`
 	Interval            int    `toml:"interval"`
 	LooptestMatchSuffix string `toml:"looptest_match_suffix"`
+	AnalystID           string
 	Log                 common.Logger
 	NatsHandle          nats
-	LibtapirHandle      libtapir
 }
 
 type appHandle struct {
@@ -28,7 +29,6 @@ type appHandle struct {
 	looptestMatchSuffix string
 	log                 common.Logger
 	natsHandle          nats
-	libtapirHandle      libtapir
 	ticker              *time.Ticker
 	exitCh              chan<- common.Exit
 	pm
@@ -45,18 +45,12 @@ type job struct {
 
 type nats interface {
 	ActivateSubscription(context.Context) (<-chan common.NatsMsg, error)
-	SetObservationLooptest(context.Context, string) error
+	SetObservation(context.Context, string, string) error
 	Shutdown() error
-}
-
-type libtapir interface { // TODO create own libtapir handle instead of injecting
-	ExtractDomain([]byte) (string, error)
-	NormalizeDomainNameSuffix(string) string
 }
 
 func Create(conf Conf) (*appHandle, error) {
 	a := new(appHandle)
-	a.id = "main app"
 
 	if conf.Log == nil {
 		return nil, common.ErrBadHandle
@@ -68,10 +62,11 @@ func Create(conf Conf) (*appHandle, error) {
 	}
 	a.natsHandle = conf.NatsHandle
 
-	if conf.LibtapirHandle == nil {
-		return nil, common.ErrBadHandle
+	if conf.AnalystID == "" {
+		a.log.Error("Bad analyst ID when creating looptest")
+		return nil, common.ErrBadParam
 	}
-	a.libtapirHandle = conf.LibtapirHandle
+	a.id = conf.AnalystID
 
 	if conf.Interval > 0 {
 		a.ticker = time.NewTicker(time.Duration(conf.Interval) * time.Second)
@@ -84,7 +79,7 @@ func Create(conf Conf) (*appHandle, error) {
 	if conf.LooptestMatchSuffix == "" {
 		a.log.Warning("No match suffix given for looptests. Will only run ticker tests")
 	}
-	a.looptestMatchSuffix = a.libtapirHandle.NormalizeDomainNameSuffix(conf.LooptestMatchSuffix) // TODO normalize suffix in libtapir to use here differenciate between domain suffix and subject suffix
+	a.looptestMatchSuffix = libtapir.NormalizeDomainNameSuffix(conf.LooptestMatchSuffix)
 
 	a.log.Debug("Main app debug logging enabled")
 	return a, nil
@@ -95,7 +90,6 @@ func (a *appHandle) Run(ctx context.Context, exitCh chan<- common.Exit) {
 
 	var natsChan <-chan common.NatsMsg
 	var err error
-	a.id = "main app"
 	a.exitCh = exitCh
 	jobChan := make(chan job, 10)
 
@@ -175,7 +169,7 @@ func (a *appHandle) handleTick(ctx context.Context, epoch int64) {
 	}
 
 	tickDomain := fmt.Sprintf("epoch-%d.ticker.looptest.dnstapir.se.", epoch)
-	err := a.natsHandle.SetObservationLooptest(ctx, tickDomain)
+	err := a.natsHandle.SetObservation(ctx, tickDomain, common.OBS_LOOPTEST)
 	if err != nil {
 		a.log.Error("Error setting looptest observation for '%s': %s", tickDomain, err)
 	}
@@ -196,180 +190,23 @@ func (a *appHandle) handleMsg(ctx context.Context, msg common.NatsMsg) {
 		return
 	}
 
-	msgDomain, err := a.libtapirHandle.ExtractDomain(msg.Data)
+	msgDomain, err := libtapir.ExtractDomain(msg.Data)
 	if err != nil {
 		a.log.Error("Error reading domain from message: %s", err)
 		return
 	}
 
-	if !strings.HasSuffix(msgDomain, "."+a.looptestMatchSuffix) {
+	if !strings.HasSuffix(msgDomain, a.looptestMatchSuffix) {
 		a.log.Debug("No match: %s", msgDomain)
 		return
 	} else {
 		a.log.Debug("Looptest domain match: %s", msgDomain)
 	}
 
-	err = a.natsHandle.SetObservationLooptest(ctx, msgDomain)
+	err = a.natsHandle.SetObservation(ctx, msgDomain, common.OBS_LOOPTEST)
 	if err != nil {
 		a.log.Error("Error setting looptest observation for '%s': %s", msgDomain, err)
 	} else {
 		a.log.Debug("Looptest observation set for '%s'", msgDomain)
 	}
 }
-
-//type App struct {
-//	Log           common.Logger
-//	Nats          nats
-//	Tapir         tapir
-//	Ticker        ticker
-//	isInitialized bool
-//	doneChan      chan error
-//	stopChan      chan bool
-//	wg            *sync.WaitGroup
-//}
-//
-//type nats interface {
-//	ActivateSubscription() (<-chan string, error)
-//	Publish(msg string) error
-//}
-//
-//type tapir interface {
-//	GenerateMsg(domain string, flags uint32) (string, error)
-//	ExtractDomain(msgJson string) (string, error)
-//}
-//
-//type ticker interface {
-//	StartTick() (<-chan int64, error)
-//	StopTick()
-//}
-//
-//func (a *App) Initialize() error {
-//	var wg sync.WaitGroup
-//	a.wg = &wg
-//
-//	a.doneChan = make(chan error, 10)
-//	a.stopChan = make(chan bool)
-//
-//	if a.Log == nil {
-//		return errors.New("no logger object")
-//	}
-//
-//	if a.Nats == nil {
-//		return errors.New("no nats object")
-//	}
-//
-//	if a.Tapir == nil {
-//		return errors.New("no tapir object")
-//	}
-//
-//	a.isInitialized = true
-//	return nil
-//}
-//
-//func (a *App) Run() <-chan error {
-//	if !a.isInitialized {
-//		panic("app not initialized")
-//	}
-//
-//	a.Log.Info("Starting main loop")
-//	a.wg.Add(1)
-//	go func() {
-//		defer a.wg.Done()
-//
-//		tickCh, err := a.Ticker.StartTick()
-//		if err != nil {
-//			a.doneChan <- errors.New("error starting ticker")
-//			return
-//		}
-//
-//        msgChan, err := a.Nats.ActivateSubscription()
-//        if err != nil {
-//            a.doneChan <- errors.New("error activating nats subscription")
-//            return
-//        }
-//
-//		for {
-//			select {
-//			case tick := <-tickCh:
-//				a.handleTick(tick)
-//			case msg := <-msgChan:
-//				a.handleMsg(msg)
-//			case <-a.stopChan:
-//				a.Log.Info("Stopping main worker thread")
-//				return
-//			}
-//		}
-//	}()
-//
-//	a.Log.Info("Application is now up and running")
-//	return a.doneChan
-//}
-//
-//func (a *App) Stop() error {
-//	if a.isInitialized {
-//		a.Log.Info("Stopping application")
-//	} else {
-//		a.Log.Info("Stop() called but application was not initialized")
-//	}
-//
-//	a.Ticker.StopTick()
-//	a.stopChan <- true
-//	a.wg.Wait()
-//
-//	close(a.doneChan)
-//	close(a.stopChan)
-//
-//	a.Log.Info("Application stopped")
-//
-//	return nil
-//}
-//
-//func (a *App) handleTick(tick int64) {
-//	a.Log.Debug("Received tick '%d'", tick)
-//	if tick == 0 {
-//		a.Log.Debug("Tick had zero value, probably garbage. Won't handle...")
-//		return
-//	}
-//
-//	tickDomain := fmt.Sprintf("epoch-%d.ticker.looptest.dnstapir.se.", tick)
-//	outMsg, err := a.Tapir.GenerateMsg(tickDomain, 2048)
-//	if err != nil {
-//		a.Log.Error("Error generating message: %s", err)
-//		return
-//	}
-//
-//	err = a.Nats.Publish(string(outMsg))
-//	if err != nil {
-//		a.Log.Error("Error publishing nats message!")
-//	}
-//}
-//
-//func (a *App) handleMsg(msg string) {
-//	a.Log.Debug("Received message '%s'", msg)
-//	if msg == "" {
-//		a.Log.Debug("Msg had zero value, probably garbage. Won't handle...")
-//		return
-//	}
-//
-//	msgDomain, err := a.Tapir.ExtractDomain(msg)
-//	if err != nil {
-//		a.Log.Error("Error reading domain from message: %s", err)
-//		return
-//	}
-//
-//    if !strings.HasSuffix(msgDomain, "from-edge.looptest.dnstapir.se.") {
-//		a.Log.Debug("Ignoring msg with domain '%s'", msgDomain)
-//        return
-//    }
-//
-//	outMsg, err := a.Tapir.GenerateMsg(msgDomain, 2048)
-//	if err != nil {
-//		a.Log.Error("Error generating message: %s", err)
-//		return
-//	}
-//
-//	err = a.Nats.Publish(string(outMsg))
-//	if err != nil {
-//		a.Log.Error("Error publishing nats message!")
-//	}
-//}
